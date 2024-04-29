c106eb84812e31e054c1f181a72e56f5f1a25c42
oebaedz <oebaedz@gmail.com>
2024-04-29 23:54:53 +0700

    fixed homepage height

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -7,13 +7,13 @@ const Homepage = (props) => {
     return (
         <>
             <Head title={props.title} />
-            <Navbar auth={props.auth.user}/>
-            <div className="min-h-screen flex justify-center gap-6 flex-wrap m-4">
+            <Navbar auth={props.auth.user} />
+            <div className="flex justify-center gap-6 flex-wrap mt-4 mb-20">
                 {props.news.data.map((item) => (
-                    <NewsCard key={item.id} item={item}/>
+                    <NewsCard key={item.id} item={item} />
                 ))}
             </div>
-            <Paginator meta={props.news.meta}/>
+            <Paginator meta={props.news.meta} />
         </>
     );
 };
