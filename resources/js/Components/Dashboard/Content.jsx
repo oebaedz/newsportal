d8d7ee9e1550fcc1769ea2dab4312905d3e00051
oebaedz <oebaedz@gmail.com>
2024-05-04 22:17:59 +0700

    Added News List in dashboard

--- resources/js/Components/Dashboard/Content.jsx
+++ resources/js/Components/Dashboard/Content.jsx
@@ -4,6 +4,15 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useToast } from "../ui/use-toast";
 
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/Components/ui/card";
+
 const Content = () => {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
@@ -18,9 +27,8 @@ const Content = () => {
     };
 
     useEffect(() => {
-        flash ? toast({description: flash}) : ""
-    }, [flash])
-
+        flash ? toast({ description: flash }) : "";
+    }, [flash]);
 
     return (
         <div className="py-12">
@@ -50,6 +58,37 @@ const Content = () => {
                             }
                         />
                         <Button onClick={() => handleSubmit()}>Tambah</Button>
+                    </div>
+                </div>
+                <div className="bg-white overflow-hidden mt-6 shadow-sm sm:rounded-lg">
+                    <div className="p-6 text-gray-900 space-y-6">
+                        <Card className="shadow-md flex flex-row">
+                            <div className="">
+                                <img
+                                    className="h-36"
+                                    src="https://i.ytimg.com/vi/nXKxU89QFvA/maxresdefault.jpg"
+                                    alt="genshin"
+                                />
+                            </div>
+                            <div className="flex flex-col items-start">
+                                <CardHeader>
+                                    <CardTitle className="truncate text-md">
+                                        {/* {item.title} */}
+                                        Title
+                                    </CardTitle>
+                                    <CardDescription>
+                                        <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+                                            {/* {item.category} */}
+                                            Cate
+                                        </span>
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    {/* <p className="line-clamp-2">{item.description}</p> */}
+                                    <p className="text-sm line-clamp-2">Desc</p>
+                                </CardContent>
+                            </div>
+                        </Card>
                     </div>
                 </div>
             </div>
